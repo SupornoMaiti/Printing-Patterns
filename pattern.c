b5c4af65ac2ab2ccecd03f615280e0c37c2fa4e1
SupornoMaiti <28supornomaiti@gmail.com>
2024-11-20 09:30:33 +0530

    New pattern added

--- pattern.c
+++ pattern.c
@@ -29,10 +29,25 @@ void pattern3(int n){
         printf("\n");
     }
 }
+void pattern4(int n){
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<i; j++){
+            printf(" ");
+        }
+        for(int k=0;k<2*n-(2*i+1);k++){
+            printf("*");
+        }
+        for(int l = 0; l<i; l++){
+            printf(" ");
+        }
+        printf("\n");
+    }
+    
+}
 int main(){
     int n;
     printf("enter a number : ");
     scanf("%d",&n);
-    pattern3(n);
+    pattern4(n);
     return 0;
 }
