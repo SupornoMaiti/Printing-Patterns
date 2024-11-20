6c7a37dd661dff63afb6a5b3f833759dc011623f
SupornoMaiti <28supornomaiti@gmail.com>
2024-11-20 11:44:09 +0530

    Added a New Pattern.

--- pattern.c
+++ pattern.c
@@ -44,10 +44,36 @@ void pattern4(int n){
     }
     
 }
+void pattern5(int n){
+    for(int i = 1; i <=n; i++){
+        for(int j = 1; j <= n-i; j++){
+            printf(" ");
+        }
+        for(int k=1;k<=2*i-1;k++){
+            printf("*");
+        }
+        for(int l = 1; l <= n-i; l++){
+            printf(" ");
+        }
+        printf("\n");
+    }
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<i; j++){
+            printf(" ");
+        }
+        for(int k=0;k<2*n-(2*i+1);k++){
+            printf("*");
+        }
+        for(int l = 0; l<i; l++){
+            printf(" ");
+        }
+        printf("\n");
+    }
+}
 int main(){
     int n;
     printf("enter a number : ");
     scanf("%d",&n);
-    pattern4(n);
+    pattern5(n);
     return 0;
 }
