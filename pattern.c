80c589672dccf8bc48f8b83863b487fe66d512db
SupornoMaiti <28supornomaiti@gmail.com>
2024-11-22 21:32:20 +0530

    Added New Functions

--- pattern.c
+++ pattern.c
@@ -59,7 +59,7 @@ void pattern5(int n){
     }
     for(int i = 0; i<n; i++){
         for(int j = 0; j<i; j++){
-            printf(" ");
+            printf(" ");                 
         }
         for(int k=0;k<2*n-(2*i+1);k++){
             printf("*");
@@ -70,10 +70,70 @@ void pattern5(int n){
         printf("\n");
     }
 }
+void pattern6(int n){
+    for (int i = 1; i <= 2 * n - 1; i++) {
+        if (i <= n) { // Upper part of the triangle
+            for (int j = 1; j <= i; j++) {
+                printf("*");
+            }
+        } else { // Lower part of the triangle
+            for (int j = 1; j <= 2 * n - i; j++) {
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+}
+void pattern7(int n){
+    int start = 0;
+    for(int i = 0; i < n; i++){
+        if(i%2 == 0){
+            start = 0;
+        }
+        else start = 1;
+    for(int j = 0;j <= i; j++){
+        printf("%d ",start);
+        start = 1-start;
+    }
+printf("\n");
+  } 
+}
+void pattern8(int n){
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            printf("%d",j);
+        }
+        for(int j = 1; j <= 2*(n-i); j++){
+            printf(" ");
+        }
+        for(int k = i; k >= 1; k-- ){
+            printf("%d",k);
+        }
+    printf("\n");    
+    }        
+}
+void pattern9(int n){
+    int p = 1;
+    for(int i = 1; i <= n; i++){
+        for(int j =1; j <= i; j++){
+            printf("%d ",p);
+            p++;
+        }
+        printf("\n");
+    }
+}
+void pattern10(int n){
+    for(int i = 0; i < n; i++){
+        for(char j = 'A';j <= 'A'+i;j++){
+            printf("%c",j);
+        }
+        printf("\n");
+    }
+}
 int main(){
-    int n;
-    printf("enter a number : ");
-    scanf("%d",&n);
-    pattern5(n);
+    int n=10;
+   // rintf("enter a number : ");
+    //scanf("%d",&n);
+    pattern10(n);
     return 0;
 }
